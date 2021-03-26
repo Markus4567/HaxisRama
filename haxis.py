26aaa3160ef4616e5f67a4c1400b439de61730f7
haxiswillow <78441034+haxiswillow@users.noreply.github.com>
2021-03-25 22:52:04 -0300

    Create haxis.py

---
+++ haxis.py
@@ -0,0 +1,12 @@
+import requests
+import time
+
+# Ler as Msgs
+while true
+ token = 
+  '1715148022:AAHrWIgTdOfLIq4-AENGPlgRncxm9sBFLlE'
+  url_base = f'https://api.telegram.org/bot{token}/getUpdates'
+  resultados = requests.get(url_base)
+  print(resultado.jsoon())
+  time.sleep (10)
+  
