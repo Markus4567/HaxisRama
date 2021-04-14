38a4d235409baadcf99c3b32d1cdead033bc5faf
Markus4567 <52223698+Markus4567@users.noreply.github.com>
2021-04-13 21:40:49 -0300

    Update haxis.py
    Não mostra resultados de mensagens repetidas e tem um intervalo menor de captura

--- haxis.py
+++ haxis.py
@@ -2,11 +2,14 @@ import requests
 import time
 
 # Ler as Msgs
-while true
+resultados_save = ''
+while True:
  token = 
   '1715148022:AAHrWIgTdOfLIq4-AENGPlgRncxm9sBFLlE'
   url_base = f'https://api.telegram.org/bot{token}/getUpdates'
   resultados = requests.get(url_base)
-  print(resultado.jsoon())
-  time.sleep (10)
+  if resultados_save != resultados:
+    print(resultado.json())
+    resultados_save = resultados
+  time.sleep (2)
   
